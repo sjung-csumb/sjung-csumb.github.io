0c6a058fffbb9e9aa3536e0ccb84154297fca211
Seokchan Jung <sjung@csumb.edu>
2025-09-14 21:41:51 -0700

    HW1

---
+++ W3/HW 1/script.js
@@ -0,0 +1,12 @@
+// Create four web pages about  ANY topic related to CS
+// (e.g., Software Development Life Cycle Methodologies, Programming Languages, Operating Systems, Virus, etc).
+
+// Requirements	Points
+
+// The remaining three pages should include more detailed information about a sub-topic. 
+// Paraphrase instead of copying the content from an external site
+
+
+
+
+// Include as part of your post the new tags and properties used.
